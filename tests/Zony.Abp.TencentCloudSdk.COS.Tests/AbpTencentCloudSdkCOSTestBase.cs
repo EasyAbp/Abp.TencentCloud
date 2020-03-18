8ee7942c655d7865373c1d613624dd8178acf69e
GameBelial <243387971@qq.com>
2020-03-19 01:18:59 +0800

    fix(COS): 添加缺失的保护成员。

--- tests/Zony.Abp.TencentCloudSdk.COS.Tests/AbpTencentCloudSdkCOSTestBase.cs
+++ tests/Zony.Abp.TencentCloudSdk.COS.Tests/AbpTencentCloudSdkCOSTestBase.cs
@@ -1,8 +1,16 @@
-﻿using Zony.Abp.TencentCloudSdk.TestBase;
+﻿using Microsoft.Extensions.Options;
+using Zony.Abp.TencentCloudSdk.Common;
+using Zony.Abp.TencentCloudSdk.TestBase;
 
 namespace Zony.Abp.TencentCloudSdk.COS.Tests
 {
     public class AbpTencentCloudSdkCOSTestBase : AbpTencentCloudSdkTestBase<AbpTencentCloudSdkCOSTestsModule>
     {
+        protected AbpTencentCloudSdkCommonOptions TencentCloudSdkCommonOptions;
+
+        public AbpTencentCloudSdkCOSTestBase()
+        {
+            TencentCloudSdkCommonOptions = GetRequiredService<IOptions<AbpTencentCloudSdkCommonOptions>>().Value;
+        }
     }
 }
