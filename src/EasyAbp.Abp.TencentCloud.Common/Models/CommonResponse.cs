3ce71c668c1a605337278a160b6a4612385eca2e
gdlcf88 <gdlcf88@outlook.com>
2020-08-11 00:49:53 +0800

    Add Error property to CommonResponse

--- src/EasyAbp.Abp.TencentCloud.Common/Models/CommonResponse.cs
+++ src/EasyAbp.Abp.TencentCloud.Common/Models/CommonResponse.cs
@@ -3,5 +3,14 @@
     public class CommonResponse : ICommonResponse
     {
         public string RequestId { get; set; }
+        
+        public ErrorModel Error { get; set; }
+    }
+
+    public class ErrorModel
+    {
+        public string Code { get; set; }
+        
+        public string Message { get; set; }
     }
 }
