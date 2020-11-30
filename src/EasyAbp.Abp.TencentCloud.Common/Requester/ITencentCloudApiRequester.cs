5dd76d611fd0bcc550700a55acab4ec45963381b
gdlcf88 <gdlcf88@outlook.com>
2020-11-30 15:55:49 +0800

    Add options param to SendRequestAsync()

--- src/EasyAbp.Abp.TencentCloud.Common/Requester/ITencentCloudApiRequester.cs
+++ src/EasyAbp.Abp.TencentCloud.Common/Requester/ITencentCloudApiRequester.cs
@@ -6,5 +6,7 @@ namespace EasyAbp.Abp.TencentCloud.Common.Requester
     public interface ITencentCloudApiRequester
     {
         Task<TResponse> SendRequestAsync<TResponse>(ICommonRequest request, string endpoint) where TResponse : ICommonResponse;
+        
+        Task<TResponse> SendRequestAsync<TResponse>(ICommonRequest request, string endpoint, AbpTencentCloudCommonOptions options) where TResponse : ICommonResponse;
     }
 }
--- src/EasyAbp.Abp.TencentCloud.Common/Requester/TencentCloudApiRequester.cs
+++ src/EasyAbp.Abp.TencentCloud.Common/Requester/TencentCloudApiRequester.cs
@@ -22,16 +22,22 @@ namespace EasyAbp.Abp.TencentCloud.Common.Requester
             _sdkCommonOptions = sdkCommonOptions.Value;
         }
 
-        public async Task<TResponse> SendRequestAsync<TResponse>(ICommonRequest request, string endpoint) where TResponse : ICommonResponse
+        public virtual async Task<TResponse> SendRequestAsync<TResponse>(ICommonRequest request, string endpoint)
+            where TResponse : ICommonResponse
+        {
+            return await SendRequestAsync<TResponse>(request, endpoint, _sdkCommonOptions);
+        }
+
+        public virtual async Task<TResponse> SendRequestAsync<TResponse>(ICommonRequest request, string endpoint,
+            AbpTencentCloudCommonOptions options) where TResponse : ICommonResponse
         {
             request.SetEndpoint(endpoint);
-            request.SetAuthorization(_sdkCommonOptions.SecretId, _sdkCommonOptions.SecretKey);
+            request.SetAuthorization(options.SecretId, options.SecretKey);
 
-            using (var response = await HttpClient.SendAsync(request.HttpRequestMessage))
-            {
-                var result = await response.Content.ReadAsStringAsync();
-                return JObject.Parse(result).SelectToken("$.Response").ToObject<TResponse>();
-            }
+            using var response = await HttpClient.SendAsync(request.HttpRequestMessage);
+            
+            var result = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(result).SelectToken("$.Response").ToObject<TResponse>();
         }
     }
 }
