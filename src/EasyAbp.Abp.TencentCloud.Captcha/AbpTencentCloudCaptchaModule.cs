869092045cd9ef6a7a675a6984b2ceb50c4a11cc
slarkerino <wanjackcool@qq.com>
2025-01-03 00:04:48 +0800

    feat: 新增验证码

---
+++ src/EasyAbp.Abp.TencentCloud.Captcha/AbpTencentCloudCaptchaModule.cs
@@ -0,0 +1,10 @@
+﻿using EasyAbp.Abp.TencentCloud.Common;
+using Volo.Abp.Modularity;
+
+namespace EasyAbp.Abp.TencentCloud.Captcha
+{
+    [DependsOn(typeof(AbpTencentCloudCommonModule))]
+    public class AbpTencentCloudCaptchaModule : AbpModule
+    {
+    }
+}
---
+++ src/EasyAbp.Abp.TencentCloud.Captcha/AbpTencentCloudCaptchaOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace EasyAbp.Abp.TencentCloud.Sms
+{
+    public class AbpTencentCloudCaptchaOptions
+    {
+        public string EndPoint { get; set; }
+        
+        public string DefaultCaptchaAppId { get; set; }
+        
+        public string DefaultAppSecretKey { get; set; }
+        
+        public string DefaultExtendCode { get; set; }
+        
+        public string DefaultSenderId { get; set; }
+
+        public AbpTencentCloudCaptchaOptions()
+        {
+            EndPoint = "captcha.tencentcloudapi.com";
+        }
+    }
+}
